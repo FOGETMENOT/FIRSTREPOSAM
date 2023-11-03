976dcc2464031a6a2f3290e88b2f2cc54f40068c
tanvi <sammiksha102@gmail.com>
2023-11-03 21:54:56 +0530

    commit in   demoblaze

---
+++ DemoBlazefirstpageobjectmodelproject/src/test/java/swagtest/logintest1.java
@@ -0,0 +1,30 @@
+package swagtest;
+
+
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+import utilities.SeleniumUtility;
+
+
+import webpagesPom.
+public class logintest1 extends SeleniumUtility {
+	loginpage login;
+	
+	@BeforeMethod
+	 public void precondition(){
+		
+	}
+	@Test
+	public void tc1()
+	{
+		
+	}
+	@AfterMethod
+	public void postcondition()
+	{
+		
+	}
+}
