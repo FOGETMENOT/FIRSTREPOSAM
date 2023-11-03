4b8f889b1ec2111f786015d897ecf51a2a2f6bf6
tanvi <sammiksha102@gmail.com>
2023-11-03 20:59:30 +0530

    commit in   swag SAM

---
+++ SwagfirstPageobjectmodelproject/src/test/java/swagtest/swagmenutest - Copy.java
@@ -0,0 +1,19 @@
+package swagtest;
+
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import utilities.SeleniumUtility;
+
+public class swagmenutest extends SeleniumUtility{
+ 
+	@BeforeMethod
+	public void precondition()
+	{
+	}
+	@Test
+	public void testmenu()
+	{
+		
+	}
+}
